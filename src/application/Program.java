79acd47962148d68c96d93db219f7ea0d35055fe
Pablo Carrijo <pablo.vcarrijo@gmail.com>
2024-05-06 16:39:40 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department department = new Department(1, "Books");
+		Department obj = new Department(1, "Books");
 		
-		System.out.println(department);
+		Seller seller = new Seller(21, "Pablo", "pablo.vcarrijo@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(obj);
+		System.out.println(seller);
 		
 	}
 	
