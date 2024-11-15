da5ea28cf4283d9e983f3c5109bd51e54de4b9f3
pablo.vcarrijo@gmail.com <pablo.vcarrijo@gmail.com>
2024-11-15 18:20:37 -0300

    DaoFactory and SellerDaoJDBC

--- src/application/App.java
+++ src/application/App.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,7 +15,9 @@ public class App {
 		
 		Seller seller = new Seller(1, "Pablo", "pablo.vcarrijo@gmail.com", new Date(), 100000.0, dep);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
+		
 		
 	}
 
