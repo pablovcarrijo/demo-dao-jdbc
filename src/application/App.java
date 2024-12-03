5080452428cc17b91f8f4518e02469d96af8c597
pablo.vcarrijo@gmail.com <pablo.vcarrijo@gmail.com>
2024-12-03 12:07:53 -0300

    Department entity class

--- src/application/App.java
+++ src/application/App.java
@@ -3,14 +3,16 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import model.entitites.Department;
 
 public class App {
 
 	public static void main(String[] args) {
 		Connection conn = DB.getConnection();
 		DB.closeConnection();
-		System.out.println("Deu certo");
-
+		
+		Department dep = new Department(1, "Books");
+		System.out.println(dep);
 	}
 
 }
